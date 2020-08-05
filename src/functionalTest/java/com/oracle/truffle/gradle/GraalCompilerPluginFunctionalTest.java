3fa7d1d77d9f5a1f402bdc6095d86b810832fafb
Samuel Pastva <daemontus@gmail.com>
2020-08-05 16:29:50 +0200

    Replace Java 11 APIs.

--- src/functionalTest/java/com/oracle/truffle/gradle/GraalCompilerPluginFunctionalTest.java
+++ src/functionalTest/java/com/oracle/truffle/gradle/GraalCompilerPluginFunctionalTest.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Objects;
 
 import static org.junit.Assert.assertFalse;
@@ -29,12 +29,12 @@ public class GraalCompilerPluginFunctionalTest {
                 - start scripts for the distribution initialize compiler and Graal JS
          */
         File projectDir = new File("build/testProject");
+        File settingsFile = new File(projectDir, "settings.gradle");
+        File buildFile = new File(projectDir, "build.gradle");
         Files.createDirectories(projectDir.toPath());
-        Files.writeString(Path.of(projectDir.getAbsolutePath(), "settings.gradle"),
-                ""
-        );
-        Files.writeString(Path.of(projectDir.getAbsolutePath(), "build.gradle"),
-                "plugins { \n" +
+        Files.write(settingsFile.toPath(), "".getBytes());
+        Files.write(buildFile.toPath(),
+                ("plugins { \n" +
                         "id 'java-library'\n" +
                         "id 'application'\n" +
                         "id 'org.graalvm.plugin.compiler'\n" +
@@ -46,7 +46,7 @@ public class GraalCompilerPluginFunctionalTest {
                     "task runApp(type: JavaExec) {\n" +
                         "classpath = sourceSets.main.runtimeClasspath\n" +
                         "main = 'package.Main'\n" +
-                    "}\n"
+                    "}\n").getBytes()
         );
 
         // Run the build
@@ -79,7 +79,7 @@ public class GraalCompilerPluginFunctionalTest {
 
         File distStartScript = new File(distBinDir, "testProject");
         assertTrue(distStartScript.exists() && !distStartScript.isDirectory());
-        String startScript = Files.readString(distStartScript.toPath());
+        String startScript = new String(Files.readAllBytes(distStartScript.toPath()), Charset.defaultCharset());
         assertTrue(startScript.contains("--upgrade-module-path=$APP_HOME/graalCompiler/"));
         assertTrue(startScript.contains("-Dtruffle.class.path.append=$APP_HOME/lib/js-20.1.0.jar"));
 
